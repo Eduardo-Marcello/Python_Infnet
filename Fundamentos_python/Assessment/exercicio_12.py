a802f5bbc5032aac0d1b9fe944b72ccdec942674
Eduardo Marcello <eduardo.duarte@soulasalle.com.br>
2021-09-25 14:20:10 -0300

    tirando um bug

--- Fundamentos_python/Assessment/exercicio_12.py
+++ Fundamentos_python/Assessment/exercicio_12.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def check_acronyms(s_def, initials_def, country_def):
+    if (initials_def == "GO") or (initials_def == "MT") or (initials_def == "MS") or (initials_def == "DF"):
+        for i in s_def.html.body.find_all("div", "linha"):
+            country_def[i.text] = i.get("linha")
+        print(country_def)
+    else:
+        print("This state does not belong to Midwest")
+
+
 url = "https://fgopassos.github.io/pagina_exemplo/estadosCentroOeste.html"
-
-html = requests.get(url).text
-soup = BeautifulSoup(html, "lxml")
-sigla = input("Insira a sigla do estado: ")
-estados = {}
-
-if (sigla == "GO") or (sigla == "MT") or (sigla == "MS") or (sigla == "DF"):
-    for div in soup.html.body.find_all("div", "linha"):
-        estados[div.text] = div.get("linha")
-    print(estados)
-else:
-    print("Estado não pertecente ao Centro-Oeste")
+dt = requests.get(url).text
+s = BeautifulSoup(dt, "lxml")
+initials = input("Enter a state abbreviation: ")
+country = {}
+check_acronyms(s, initials, country)
--- Fundamentos_python/Assessment/exercicio_13.py
+++ Fundamentos_python/Assessment/exercicio_13.py
@@ -2,27 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 
+
+def count_words(single_word_def, text_def):
+    for single_word_def in [p.text for p in single_word_def]:
+        single_word_def = single_word_def.strip()
+        text_def += single_word_def.lower().replace(",", "").replace(".", "") + " "
+    count_def = Counter(text_def.split(" "))
+    print(f"There are {len(count_def.items())} words in the text")
+    return count_def
+
+
+def find_single_words(count_def2, single_word_def2):
+    for i, value in enumerate(count_def2.values()):
+        if i == "ladies":
+            print(f"The word {i} appears {value} times")
+        if value == 1:
+            single_word_def2.append(count_def2.get(value))
+    print(f"The unique words that appear within the text:\n{single_word_def2}")
+
+
 url = "http://brasil.pyladies.com/about"
-
-html = requests.get(url).text
-soup = BeautifulSoup(html, "lxml", from_encoding="utf-8")
-
-paragrafos = soup.find_all('p')
-
-palavras_unicas = []
-
-texto = ""
-
-for paragrafo in [p.text for p in paragrafos]:
-    paragrafo = paragrafo.strip()
-    texto += paragrafo.lower().replace(",", "").replace(".", "") + " "
-
-count = Counter(texto.split(" "))
-print("São", len(count.items()), "palavras no texto")
-
-for key, value in count.items():
-    if key == "ladies":
-        print("A palavra ", key, "aparece nesta quantidade:", value)
-    if value == 1:
-        palavras_unicas.append(key)
-print("Palavras únicas que aparecem dentro do texto:\n", palavras_unicas)
+dt = requests.get(url)
+dt.encoding = dt.apparent_encoding
+s = BeautifulSoup(dt.text, "lxml", from_encoding="utf-8")
+Lines = s.find_all('p')
+single_word = []
+text = ""
+count = count_words(single_word, text)
+find_single_words(count, single_word)
